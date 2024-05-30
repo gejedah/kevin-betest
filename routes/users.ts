f7a96170355e2eb2f357ee80979526295fbe7e6e
Calvin <gejedah@users.noreply.github.com>
2024-05-30 15:09:33 +0000

    default anonim

--- routes/users.ts
+++ routes/users.ts
@@ -8,11 +8,21 @@ router.get('/', function(req: express.Request, res: express.Response, next: expr
   res.send('respond with a resource');
 });
 router.post('/users/create', createMongoDataController);
-router.get('/users', createMongoDataController);
-router.get('/users/account/:number', createMongoDataController);
-router.get('/users/identity/:number', createMongoDataController);
-router.get('/users/:id', createMongoDataController);
+router.get('/users', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.send('respond with a resource');
+});
+router.get('/users/account/:number', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.send('respond with a resource');
+});
+router.get('/users/identity/:number', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.send('respond with a resource');
+});
+router.get('/users/:id', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.send('respond with a resource');
+});
 router.put('/users/:id/update', updateMongoDataController);
-router.delete('/users/:id/remove', createMongoDataController);
+router.delete('/users/:id/remove', function(req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;
