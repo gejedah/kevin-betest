2a7d2eba726a43b1bc24fa1d43953574fa580e4c
Calvin <gejedah@users.noreply.github.com>
2024-05-30 08:37:34 +0000

    fix require

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-import * as indexRouter from './routes/index';
-import * as usersRouter from './routes/users';
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
